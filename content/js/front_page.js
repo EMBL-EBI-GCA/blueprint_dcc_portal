27fd3a17eaa0595a627c9eacf647f851d7871793
davidrichardson <david.m.richardson@gmail.com>
2014-07-22 14:14:19 +0100

    show values in pie chart (rather than %)

--- content/js/front_page.js
+++ content/js/front_page.js
@@ -5,25 +5,28 @@ $().ready(function() {
     var counts = [];
 
     for (var i = 0; i < labels.length; i++) {
-      var count = [labels[i],data[labels[i]]];
+      var count = [labels[i], data[labels[i]]];
       counts[i] = count;
     }
 
-    var plot1 = jQuery.jqplot ('chart_container', [counts], 
-      { 
-        seriesDefaults: {
-          // Make this a pie chart.
-          renderer: jQuery.jqplot.PieRenderer, 
-          rendererOptions: {
-            // Put data labels on the pie slices.
-            // By default, labels show the percentage of the slice.
-            showDataLabels: true
-          }
-        }, 
-        legend: { show:true, location: 'e' },
-        title: 'Data summary'
-      }
-    );
+    // add chart
+    var plot1 = jQuery.jqplot('chart_container', [counts], {
+      seriesDefaults: {
+        // Make this a pie chart.
+        renderer: jQuery.jqplot.PieRenderer,
+        rendererOptions: {
+          // Put data labels on the pie slices.
+          // By default, labels show the percentage of the slice.
+          showDataLabels: true,
+          dataLabels: 'value',
+        }
+      },
+      legend: {
+        show: true,
+        location: 'w'
+      },
+      title: 'Data summary'
+    });
 
   });
 
