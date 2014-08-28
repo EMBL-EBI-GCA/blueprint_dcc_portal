9a6ae32543f619f565ce92e28742528df7f10c4e
davidrichardson <david.m.richardson@gmail.com>
2014-08-28 09:28:12 +0100

    Highlight active link in nav bar

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,46 +4,53 @@
 
 var controllers = angular.module('dccPortal.controllers', []);
 
+controllers.controller('NavController', ['$scope', '$location',
+  function($scope, $location, $route) {
+    $scope.activeLink = function(viewLocation) {
+      return viewLocation == $location.path();
+    };
+  }
+]);
+
 /* Experiment List */
-controllers.controller('ExperimentListCtrl', ['$scope', 'List','sharedProperty',
-  function($scope, List,sharedProperty) {
+controllers.controller('ExperimentListCtrl', ['$scope', 'List', 'sharedProperty',
+  function($scope, List, sharedProperty) {
     $scope.items = List.query({
       name: 'experiments'
     });
     $scope.items.$promise.then(function(data) {
       $scope.items = data;
     });
-    $scope.datatypes = ["Bisulfite-Seq", "DNase-Seq", "RNA-Seq", "ChIP Input", "H3K4me3", "H3K4me1", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3","H2A.Zac","H3K9/14ac"];
-    
+    $scope.datatypes = ["Bisulfite-Seq", "DNase-Seq", "RNA-Seq", "ChIP Input", "H3K4me3", "H3K4me1", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3", "H2A.Zac", "H3K9/14ac"];
+
     $scope.initialExpValues = [];
     if (sharedProperty.getProperty() && sharedProperty.getProperty() == 'ChIP-Seq') {
-      $scope.initialExpValues = ["ChIP Input", "H3K4me3", "H3K4me1", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3","H2A.Zac","H3K9/14ac"];
-    }
-    else if (sharedProperty.getProperty()) {
+      $scope.initialExpValues = ["ChIP Input", "H3K4me3", "H3K4me1", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3", "H2A.Zac", "H3K9/14ac"];
+    } else if (sharedProperty.getProperty()) {
       $scope.initialExpValues.push(sharedProperty.getProperty());
     }
     sharedProperty.clearProperty();
-    
+
     $scope.orderProp = 'SAMPLE_DESC_1';
     $scope.numPages = 5;
     $scope.pageSize = 35;
     $scope.currentPage = 1;
-    $scope.order = function(order){
-      if ($scope.orderProp == order){
-        $scope.orderProp = '-'+order;
-      }
-      else {
+    $scope.order = function(order) {
+      if ($scope.orderProp == order) {
+        $scope.orderProp = '-' + order;
+      } else {
         $scope.orderProp = order;
       }
     };
   }
 ]);
 
-controllers.controller('ExperimentDetailCtrl', ['$scope', '$routeParams', 'Item', function($scope, $routeParams, Item) {
+controllers.controller('ExperimentDetailCtrl', ['$scope', '$routeParams', 'Item',
+  function($scope, $routeParams, Item) {
     $scope.data = Item.get({
       name: $routeParams.experimentId
     });
-    $scope.data.$promise.then(function(exp){
+    $scope.data.$promise.then(function(exp) {
       $scope.data = exp;
     });
   }
@@ -62,20 +69,19 @@ controllers.controller('FileListCtrl', ['$scope', 'List',
     $scope.numPages = 5;
     $scope.pageSize = 35;
     $scope.currentPage = 1;
-    $scope.order = function(order){
-      if ($scope.orderProp == order){
-        $scope.orderProp = '-'+order;
-      }
-      else {
+    $scope.order = function(order) {
+      if ($scope.orderProp == order) {
+        $scope.orderProp = '-' + order;
+      } else {
         $scope.orderProp = order;
       }
-    } 
+    }
   }
 ]);
 
 /* Home */
-controllers.controller('HomeCtrl', ['$scope', 'List', 'Item','Modernizr','$location','sharedProperty',
-  function($scope, List, Item, Modernizr,$location,sharedProperty) {
+controllers.controller('HomeCtrl', ['$scope', 'List', 'Item', 'Modernizr', '$location', 'sharedProperty',
+  function($scope, List, Item, Modernizr, $location, sharedProperty) {
     $scope.counts = {
       terms: List.query({
         name: 'experiment_count'
@@ -98,8 +104,8 @@ controllers.controller('HomeCtrl', ['$scope', 'List', 'Item','Modernizr','$locat
         terms: data
       };
     });
-    
-    $scope.hackety = function(field, term){
+
+    $scope.hackety = function(field, term) {
       sharedProperty.setProperty(term);
       $location.path('experiments');
     }
